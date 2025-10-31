775aac8757f92853413a315b93e6c25168cf510f
Joel Capitao <jcapitao@redhat.com>
2025-10-31 23:12:07 +0100

    Add toolbox containerfile

---
+++ README.md
@@ -0,0 +1,5 @@
+# My personal Containerfiles
+
+The images are hosted under the [jcapitao](https://quay.io/user/jcapitao) Quay namespace.
+
+This repo is heavily based on this [repo](https://github.com/travier/quay-containerfiles).
---
+++ update_workflows.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Re-generate the GitHub workflows based on a template. We do not use a matrix
+# build strategy in GitHub worflows to be able to condition each build job on
+# changes to relevant files.
+
+set -euo pipefail
+# set -x
+
+main() {
+    if [[ ! -d .github ]] || [[ ! -d .git ]]; then
+        echo "This script must be run at the root of the repo"
+        exit 1
+    fi
+
+    apps=()
+    toolboxes=(
+        'toolbox'
+    )
+
+    for c in "${apps[@]}"; do
+        generate "${c}" ".github_workflows_template.yaml"
+    done
+    for c in "${toolboxes[@]}"; do
+        generate "${c}" ".github_workflows_template_toolbox.yaml"
+    done
+}
+
+generate() {
+    local -r name="${1}"
+    local -r template="${2}"
+
+    {
+    sed \
+        -e "s|%%NAME%%|${name}|g" \
+        "${template}"
+    } > ".github/workflows/${name}.yaml"
+}
+
+main "${@}"
